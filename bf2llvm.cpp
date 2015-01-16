1b2f1ba4b6391ff03942d55e12eeb7adfb038019
Akihiro Hayashi <ahayashi10@gmail.com>
2015-01-16 01:55:50 -0600

    make small modification to bf2llvm.cpp

--- bf2llvm.cpp
+++ bf2llvm.cpp
@@ -1,7 +1,7 @@
 #include <stack>
-#include <llvm/IR/LLVMContext.h>
-#include <llvm/IR/Module.h>
-#include <llvm/IR/IRBuilder.h>
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/IRBuilder.h"
 
 using namespace llvm;
  
